287f197705b8756ed9b53422cc9ee60c3af35daf
panpathfinder <panpathfinder@gmail.com>
2014-12-06 09:25:51 -0800

    Poll to get degree.

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -45,8 +45,35 @@ angular.module('starter.controllers', [])
             }
         };
 	})
-	.controller('MapCtrl', function($scope, $stateParams, $cordovaDeviceOrientation) {
-		var infoAr = $stateParams.mapId.split("f");
+	.controller('MapCtrl', function($scope, $stateParams, $cordovaDeviceOrientation, $timeout) {
+        $scope.truthyVal=false;
+        var pollTimer;
+        var poll = function() {
+            pollTimer = $timeout(function() {
+                console.log("polling");
+                poll();
+            }, 1000);
+        };
+        //poll();
+
+
+        /*var locationTimer;
+        var drawLocation = function() {
+            pollTimer = $timeout(function() {
+                console.log($scope.truthyVal);
+                ($scope.truthyVal)?$scope.truthyVal=false:$scope.truthyVal=true;
+                drawLocation();
+            }, 2000);
+        };
+        drawLocation();
+        $scope.$on(
+            "$destroy",
+            function( event ) {
+                $timeout.cancel(pollTimer);
+                $timeout.cancel(locationTimer);
+            }
+        );*/
+        var infoAr = $stateParams.mapId.split("f");
         $scope.mapImageUrl = (infoAr[0] == 1)? '4301':(infoAr == 2)?'4302':'4301';
         $scope.mapImageUrl += 'f' + infoAr[1] + '.png';
         $scope.angle = 30;
