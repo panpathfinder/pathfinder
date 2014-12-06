3982714fe420e245f2e201b4a4d3f78b81bc8f8a
Johnny Xie <johnny.xie@gmail.com>
2014-12-06 02:54:32 -0800

    aaa

--- mobile/www/js/ng-cordova.js
+++ mobile/www/js/ng-cordova.js
@@ -2,58 +2,9 @@
  * Created by jxie on 12/6/14.
  */
 
-angular.module('ngCordova', [
-    'ngCordova.plugins'
-]);
-
-angular.module('ngCordova.plugins', [
-    'deviceMotion',
-    'deviceOrientation'
-]);
 
 //#### Begin Individual Plugin Code####
 
-// install   :     cordova plugin add org.apache.cordova.device-motion
-// link      :     https://github.com/apache/cordova-plugin-device-motion/blob/master/doc/index.md
-
-angular.module('ngCordova.plugins.deviceMotion', [])
-
-    .factory('$cordovaDeviceMotion', ['$q', function ($q) {
-
-        return {
-            getCurrentAcceleration: function () {
-                var q = $q.defer();
-
-                navigator.accelerometer.getCurrentAcceleration(function (result) {
-                    q.resolve(result);
-                }, function (err) {
-                    q.reject(err);
-                });
-
-                return q.promise;
-            },
-
-            watchAcceleration: function (options) {
-                var q = $q.defer();
-
-                var watchId = navigator.accelerometer.watchAcceleration(function (result) {
-                    //q.resolve(watchID);
-                    q.notify(result);
-                }, function (err) {
-                    q.reject(err);
-                }, options);
-
-                return {
-                    watchId: watchId,
-                    promise: q.promise
-                };
-            },
-
-            clearWatch: function (watchID) {
-                return navigator.accelerometer.clearWatch(watchID);
-            }
-        };
-    }]);
 // install   :     cordova plugin add org.apache.cordova.device-orientation
 // link      :     https://github.com/apache/cordova-plugin-device-orientation/blob/master/doc/index.md
 
