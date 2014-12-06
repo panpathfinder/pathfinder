d09908c22fd14fe426cee88f857ee8cd92bfa909
Raghu Kanadam <rkanadam@panm18k1fh00hq.paloaltonetworks.local>
2014-12-06 09:21:42 -0800

    Adding location plugin

---
+++ mobile/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,15 @@
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes.dex : \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/com/ionicframework/myproject438760/BuildConfig.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/com/ionicframework/myproject438760/CordovaApp.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/com/ionicframework/myproject438760/R$attr.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/com/ionicframework/myproject438760/R$drawable.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/com/ionicframework/myproject438760/R$string.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/com/ionicframework/myproject438760/R$xml.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/com/ionicframework/myproject438760/R.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/org/apache/cordova/deviceorientation/CompassListener$1.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/org/apache/cordova/deviceorientation/CompassListener.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/org/apache/cordova/test/Echo$1.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/org/apache/cordova/test/Echo$2.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/org/apache/cordova/test/Echo$3.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/classes/org/apache/cordova/test/Echo.class \
+/Users/rkanadam/hackathon/pathfinder/mobile/platforms/android/ant-build/dexedLibs/classes-e955e5f3965761e16bcfc1ef34265e1c.jar \
