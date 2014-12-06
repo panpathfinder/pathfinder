6fdedf3ab1ae2b28a336323ee07e2aae6f08c17c
Johnny Xie <johnny.xie@gmail.com>
2014-12-05 20:02:32 -0800

    autocomplete

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -1,15 +1,26 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-})
+    .controller('DashCtrl', function($scope) {
+    })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+    .controller('FriendsCtrl', function($scope, Friends) {
+        $scope.friends = Friends.all();
+    })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+    .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+        $scope.friend = Friends.get($stateParams.friendId);
+    })
 
-.controller('PathFinderCtrl', function($scope) {
-});
+    .controller('PathFinderCtrl', function($scope, LDAP, SearchService) {
+        $scope.ldapDB = LDAP.get();
+
+        $scope.data = { "names" : [], "search" : '' };
+
+        $scope.search = function() {
+            SearchService.searchNames($scope.data.search).then(
+                function(matches) {
+                    $scope.data.names = matches;
+                }
+            )
+        }
+    });
