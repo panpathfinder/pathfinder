ac2b798b336bdfe50bb74bbb5224818b124f94dc
panpathfinder <panpathfinder@gmail.com>
2014-12-05 23:04:21 -0800

    web version with dummy data

--- server/js/roomfinder.js
+++ server/js/roomfinder.js
@@ -2,44 +2,113 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-$(document).ready(function() {
-    console.log('ready')
-    // Optimalisation: Store the references outside the event handler:
+$(document).ready(function () {
+    var floor = 1;
+    var building = 1;
+
+    $search = $("#search");
+    $searched = $("#searched");
+
+    var searchChanged = function () {
+        var val = $search.val();
+        console.log(val);
+        if (ldapdb[val]) {
+            var entry = ldapdb[val];
+            console.log(entry['building']);
+            console.log(entry['floor']);
+            if (entry['building'] && entry['floor']) {
+                floor = entry['floor'];
+                building = entry['building'];
+                resizeMap();
+            }
+            var label;
+            if (entry['rooms']) {
+                label = entry.display + ' GAP' + entry.building + '/' + entry['floor'] + ' (' + entry.rooms + ')';
+                if (entry["has_projector"]) {
+                    label += " Projector";
+                }
+            } else {
+                label = entry.display + ' GAP' + entry.building + '/' + entry['floor'] + ' (' + entry.cube + ') ' + entry['phone'];
+            }
+            $searched.text(label);
+
+        }
+    };
+
+    $search.autocomplete({
+        source: ldapkeys
+    });
+//    $search.keyup(searchChanged);
+    $search.keypress(searchChanged);
+    $search.change(searchChanged);
+    $search.blur(searchChanged);
+//    $search.focus(searchChanged);
+    $search.on('autocompleteselect', searchChanged);
+
+
+    var getMapImageSource = function () {
+        var src = '/img/4' + (building == 1 ? '3' : '4') + '01' + 'f' + floor + '.png';
+        console.log('image = ' + src);
+        return src;
+    };
+
     var $window = $(window);
-    function checkWidth() {
-        console.log('checkWidth');
+    var $toolbar = $('#toolbar');
+
+    var resizeMap = function () {
+        console.log('resizeMap');
+        var tw = $toolbar.width();
+        var th = $toolbar.height();
         var w = $window.width();
-        var h = $window.height();
+        var padding = 5;
+        var h = $window.height() - th - padding;
         console.log(w);
         console.log(h);
+        var paper = Raphael(0, th, w, h);
+        paper.image(getMapImageSource(), 0, 0, w, h);
+        var r = 12;
+        var x = getRandomInt(2 * r, w - 2 * r);
+        var y = getRandomInt(2 * r, h - 2 * r);
+        var star = paper.star(paper, x, y, r).attr({
+            fill: "#00FF00",
+            'stroke-width': 1
+        });
+        rect = paper.rect(30, 30, 90, 90);
+        rect.attr({
+            stroke: "none",
+            "fill-opacity": 0.05,
+            fill: "#f00"
+        });
+        rect.hover(function () {
+                rect.attr({"stroke": "#000"});
+            },
+            function () {
+                rect.attr({"stroke": "none"});
+            }
+        );
+        $(rect.node).qtip({
+            content: {
+                text: 'name: mac<br/>cube: 5566'
+            }
+        })
 
-        var paper = Raphael(0, 0, w, h);
-        paper.image('/img/4301f5.png', 0, 0, w, h);
-        var r = 15;
-        var x = getRandomInt(2*r, w-2*r);
-        var y = getRandomInt(2*r, h-2*r);
-        console.log(x)
-        console.log(y)
-//        var star = paper.star(x, y, r, r / 2, 5);
-//        console.log(star)
-//        star.attr({ fill: "#79DE00" });
-paper.star(paper, x, y, r).attr({
-//    fill: "#F90900",
-//    fill: "yellow",
-    fill: "#00FF00",
-    'stroke-width': 1
-});
-
-//        if (windowsize > 440) {
-//            //if the window is greater than 440px wide then turn on jScrollPane..
-//            $pane.jScrollPane({
-//                scrollbarWidth:15,
-//                scrollbarMargin:52
-//            });
-//        }
+    };
+    resizeMap();
+    $(window).resize(resizeMap);
+    for (var b = 1; b <= 2; b++) {
+        (function (f) {
+            $('#GAP' + f).on('click', function () {
+                building = f;
+                resizeMap();
+            });
+        })(b);
     }
-    // Execute on load
-    checkWidth();
-    // Bind event listener
-    $(window).resize(checkWidth);
+    for (var f = 1; f <= 5; f++) {
+        (function (f) {
+            $('#floor' + f).on('click', function () {
+                floor = f;
+                resizeMap();
+            });
+        })(f);
+    }
 });
